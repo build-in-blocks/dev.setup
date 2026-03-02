1b7e7d8501cfb38fbcad4f88782c6e964f8bb114
Ifycode <marychord@gmail.com>
2026-03-02 02:44:08 +0100

    Add: Setup eslint to work with typescript files

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+
+// @ts-check
+
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  tseslint.configs.stylistic,
+  {
+    rules: {
+      'no-console': 'warn',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'no-trailing-spaces': 'error',
+      'quotes': ['error', 'single'],
+      'indent': ['error', 2],
+      'camelcase': 'error'
+    }
+  },
+);
