54a04fbf99ef8e220c594d8664f3c38167ee88f6
Ifycode <marychord@gmail.com>
2026-03-04 05:05:44 +0100

    Add: Setup prettier to format js, ts, html & css files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@
 import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default defineConfig(
   eslint.configs.recommended,
@@ -16,4 +17,5 @@ export default defineConfig(
       camelcase: 'error',
     },
   },
+  eslintConfigPrettier,
 );
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+export default {
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  endOfLine: 'lf',
+};
