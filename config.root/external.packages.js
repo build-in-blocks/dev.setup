dd868ddc7dfd5a892bc56a2f11641e7444d7aed3
Ifycode <marychord@gmail.com>
2026-04-08 18:05:08 +0100

    Fix: Centralize package imports and root exports

---
+++ config.root/external.packages.js
@@ -0,0 +1,26 @@
+//-------------
+// Node-related
+//-------------
+
+import fs from 'fs';
+import path from 'path';
+import { execSync } from 'child_process';
+import process from 'process';
+import { fileURLToPath } from 'url';
+import { createRequire } from 'module';
+
+export {
+  //-
+  fs,
+  //-
+  path,
+  //-
+  execSync,
+  //-
+  process,
+  //-
+  fileURLToPath,
+  //-
+  createRequire,
+  //-
+};
