f4e36bac78f81c38b42d3823234d2b5d62820954
Ifycode <marychord@gmail.com>
2026-04-08 19:07:13 +0100

    Fix: Prettier fix on git commit (test - 11)

--- config.programmed/lint-staged.config.mjs
+++ config.programmed/lint-staged.config.mjs
@@ -1,13 +1,11 @@
 import { path, process } from '../config.root/external.packages.js';
 import { binPath } from '../config.root/root.js';
 
-// TODO: Prettier doesn't affect this particular file (on git commit - not sure why)
-
 const userAppRoot = process.cwd();
 
 const resolveBin = ({ pkgName, binRelativePath }) => {
-    const _binPath = binPath({ pkgName, binSubPath: binRelativePath });
-    return `node ${_binPath}`;
+  const _binPath = binPath({ pkgName, binSubPath: binRelativePath });
+  return `node ${_binPath}`;
 };
 
 const eslint = resolveBin({ pkgName: 'eslint', binRelativePath: 'bin/eslint.js' });
@@ -19,33 +17,13 @@ const prettier = resolveBin({ pkgName: 'prettier', binRelativePath: 'bin/prettie
 const userEslintConfig = path.resolve(userAppRoot, 'eslint.config.mjs');
 const userPrettierConfig = path.resolve(userAppRoot, 'prettier.config.mjs');
 
-const formatFiles = (filenames) => filenames.map(f => `"${f}"`).join(' ');
-
 export default {
-    '*.{mjs,js,ts}': (filenames) => {
-        const files = formatFiles(filenames);
-        return [
-        `${eslint} --config "${userEslintConfig}" --fix --no-warn-ignored ${files}`,
-        `${prettier} --config "${userPrettierConfig}" --write --ignore-unknown ${files}`,
-        ];
-    },
-    '*.{json,html,css,scss,md}': (filenames) => {
-        const files = formatFiles(filenames);
-        return [
-            `${prettier} --config "${userPrettierConfig}" --write --ignore-unknown ${files}`
-        ];
-    }
+  '*.{mjs,js,ts}': [
+    //-------------------------------------------------
+    // Using absolute paths to binaries to avoid ENOENT
+    //-------------------------------------------------
+    `${eslint} --config "${userEslintConfig}" --fix --no-warn-ignored`,
+    `${prettier} --config "${userPrettierConfig}" --write --ignore-unknown`,
+  ],
+  '*.{json,html,css,scss}': [`${prettier} --config "${userPrettierConfig}" --write --ignore-unknown`],
 };
-
-// export default {
-//     '*.{mjs,js,ts}': [
-//         //-------------------------------------------------
-//         // Using absolute paths to binaries to avoid ENOENT
-//         //-------------------------------------------------
-//         `${eslint} --config "${userEslintConfig}" --fix --no-warn-ignored`,
-//         `${prettier} --config "${userPrettierConfig}" --write --ignore-unknown`,
-//     ],
-//     '*.{json,html,css,scss}': [
-//         `${prettier} --config "${userPrettierConfig}" --write --ignore-unknown`,
-//     ]
-// };
